30c80ff9879916fafdecc9bdb17f709e60ba9e9a
VictorPullzz <GORPONFFFFFF@yandex.ru>
2018-02-21 01:16:13 +0400

    Create

---
+++ dev/js/sunsochi.js
@@ -0,0 +1,14 @@
+(function($) {
+	var SUNSOCHI = (function() {
+
+		var $sel = {};
+		$sel.window = $(window);
+		$sel.html = $("html");
+		$sel.body = $("body", $sel.html);
+
+		return {
+
+		};
+
+	})();
+})(jQuery);
